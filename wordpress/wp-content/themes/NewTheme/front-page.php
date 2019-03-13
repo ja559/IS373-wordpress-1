6fa45ed01432ae955e499386baf5fcbd689dbad7
JeremyAppelbaum <ja559@njit.edu>
2019-03-13 11:44:10 -0400

    First Commit

---
+++ wordpress/wp-content/themes/NewTheme/front-page.php
@@ -0,0 +1,25 @@
+<?php get_header();?>
+
+<div class="container">
+
+    <div class="row">
+
+
+
+        <div class="col">
+            Left Side
+        </div>
+
+
+
+        <div class="col">
+            Right Side
+        </div>
+
+
+
+    </div>
+
+</div>
+
+<?php get_footer();?>
