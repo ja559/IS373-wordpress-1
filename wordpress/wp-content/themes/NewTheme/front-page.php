b3cb4c367e0af95e24a7c73d7548aadefdf729e0
JeremyAppelbaum <ja559@njit.edu>
2019-03-23 22:53:34 -0400

    Fourth Commit: finish website tutorial.

--- wordpress/wp-content/themes/NewTheme/front-page.php
+++ wordpress/wp-content/themes/NewTheme/front-page.php
@@ -1,25 +1,38 @@
 <?php get_header();?>
 
 <div class="container pt-5 pb-5">
-
-    <div class="row">
-
-
-
-        <div class="col">
-            Left Side
-        </div>
-
-
-
-        <div class="col">
-            Right Side
-        </div>
-
-
-
-    </div>
+<div>
     <h1><?php the_title();?></h1>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <?php the_content() ?>
+    <?php endwhile; endif; ?>
 </div>
 
+
+<?php
+query_posts('posts_per_page=1');
+while(have_posts()) : the_post(); ?>
+    <div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p><?php the_excerpt(); ?></p>
+    </div>
+<?php endwhile; wp_reset_query(); ?>
+
+<div class="panel panel-default panel-body">
+    <div>
+        <div>
+            <ul>
+                <?php wp_list_categories('orderby=name&title_li='); ?>
+            </ul>
+        </div>
+    </div>
+</div>
+    <div>
+        <?php while(have_posts()) : the_post(); ?>
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <p><?php the_excerpt(); ?></p>
+        <p> posted by <?php the_author(); ?>
+            <?php endwhile; wp_reset_query(); ?>
+    </div>
+</div>
 <?php get_footer();?>
