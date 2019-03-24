9f4f2bf28d8196ba5620e04fef57360b8e6068cd
JeremyAppelbaum <ja559@njit.edu>
2019-03-23 22:53:34 -0400

    Fourth Commit: finish website tutorial.

--- wordpress/wp-content/themes/NewTheme/header.php
+++ wordpress/wp-content/themes/NewTheme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Bootstrap WordPress Theme</title>
+    <title><?php wp_title(' | ',true,'right');?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php get_stylesheet_uri(); ?>
     <?php wp_head(); ?>
