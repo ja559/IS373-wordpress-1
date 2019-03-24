2c2ae1594026a44c8769f8e2a15451471f17fa81
JeremyAppelbaum <ja559@njit.edu>
2019-03-23 23:33:42 -0400

    Fifth Commit: finalize basic of front page theme.

--- wordpress/wp-content/themes/NewTheme/front-page.php
+++ wordpress/wp-content/themes/NewTheme/front-page.php
@@ -1,32 +1,31 @@
 <?php get_header();?>
 
-<div class="container pt-5 pb-5">
-<div>
-    <h1><?php the_title();?></h1>
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <?php the_content() ?>
-    <?php endwhile; endif; ?>
-</div>
+<div class="container pt-5">
 
 
-<?php
-query_posts('posts_per_page=1');
-while(have_posts()) : the_post(); ?>
-    <div>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <p><?php the_excerpt(); ?></p>
-    </div>
-<?php endwhile; wp_reset_query(); ?>
 
-<div class="panel panel-default panel-body">
-    <div>
-        <div>
-            <ul>
-                <?php wp_list_categories('orderby=name&title_li='); ?>
-            </ul>
+    <div class="panel panel-default panel-body jumbotron">
+        <div class = "row">
+            <div class = "col-md-2">
+                <ul>
+                    <?php wp_list_categories('orderby=name&title_li='); ?>
+                </ul>
+            </div>
+            <div class = "col-md-10">
+                <?php
+                query_posts('posts_per_page=1');
+                while(have_posts()) : the_post(); ?>
+                    <div>
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <p><?php the_excerpt(); ?></p>
+                    </div>
+                <?php endwhile; wp_reset_query(); ?>
+            </div>
         </div>
     </div>
-</div>
+
+
+
     <div>
         <?php while(have_posts()) : the_post(); ?>
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -34,5 +33,7 @@ while(have_posts()) : the_post(); ?>
         <p> posted by <?php the_author(); ?>
             <?php endwhile; wp_reset_query(); ?>
     </div>
+
+
 </div>
 <?php get_footer();?>
